7c777a6c8be86c9a89f02f6fcc81b745593ac209
Ian <iangimenobarea@gmail.com>
2024-10-21 08:56:37 +0200

    codigo requerido

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,7 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("que tal");
         System.out.println("como estas?");
+        System.out.println("bien, gracias!");
+        System.out.println("Me alegro!");
     }
 }
